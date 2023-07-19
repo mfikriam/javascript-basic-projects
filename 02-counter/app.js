a05b55c2e44e3aadcd88630c0e8fe73997e8ce6c
mfikriam <yapkun17@gmail.com>
2023-07-20 00:09:29 +0800

    add functionality

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,3 +1,37 @@
 'use strict';
 
 //************** SELECTING ELEMENTS **************/
+const valueEl = document.querySelector('.value');
+const buttonsEl = document.querySelectorAll('.btn');
+
+//************** STARTING CONDITIONS **************/
+let count = 0;
+
+//************** BUTTONS FUNCTIONALITY **************/
+buttonsEl.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const styles = e.currentTarget.classList;
+
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      valueEl.style.color = 'green';
+    }
+
+    if (count < 0) {
+      valueEl.style.color = 'red';
+    }
+
+    if (count === 0) {
+      valueEl.style.color = '#222';
+    }
+
+    valueEl.textContent = count;
+  });
+});
